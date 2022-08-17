16c7074d04902215fc12e32b1d2d4bf312ac879b
pawel150199 <41188005+pawel150199@users.noreply.github.com>
2022-08-17 17:06:40 +0200

    Update MCCTest.py

--- Test/MCCTest.py
+++ Test/MCCTest.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 import numpy as np
 from sklearn.datasets import make_classification
-sys.path.append("/Users/pawelpolski/Desktop/ModifiedClusterCentroids/Main")
+sys.path.append("/Users/pawelpolski/Desktop/ModifiedClusterCentroids/Main/")
 from  ModifiedClusterCentroids import ModifiedClusterCentroids
 
 X,y = make_classification(
